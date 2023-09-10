a0b01299fbed69f4703a2a6d72dfe61f8d656eef
abuhasanmahmud <mahmud.me420@gmail.com>
2023-09-10 18:44:19 +0600

    feat(v-1) product add

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -24,7 +24,7 @@ const Products = () => {
 
   useEffect(() => {
     featchProduct();
-  }, [productAdd, deleteSuccessFully, products]);
+  }, [productAdd, deleteSuccessFully]);
   console.log("porducts", products);
 
   const updateProduct = async (product) => {
